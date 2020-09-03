eb1c32fdfa11e8fe94d865ff62579748bd11b9a7
Tenzin Sherpa <TenzinSherpa@tenzins-air.hamilton.edu>
2020-09-03 16:22:16 -0400

    completion of Activity 1

--- Activity1.R
+++ Activity1.R
@@ -1,2 +1,13 @@
 #activity1
 print("hello R world")
+#answers to question #3
+#first calculation is below
+2*2
+#create a variable for the second calculation
+x<- 567
+#now enter that variable into a mathematical operation to compute an answer.
+x*2
+#third calculation for question 3 with a vector of 4 numbers and variable
+b<-c(34, 17, 938, 38274 )
+b/2
+
