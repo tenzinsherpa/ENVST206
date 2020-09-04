d0743f4eb26e1ef9adc02cced1fbd223f24beaa8
Tenzin Sherpa <TenzinSherpa@tenzins-air.hamilton.edu>
2020-09-04 10:28:50 -0400

    demonstration in class

--- Activity1.R
+++ Activity1.R
@@ -8,6 +8,6 @@ x<- 567
 #now enter that variable into a mathematical operation to compute an answer.
 x*2
 #third calculation for question 3 with a vector of 4 numbers and variable
-b<-c(34, 17, 938, 38274 )
+b<-c(34, 17, 938, 38274)
 b/2
 
